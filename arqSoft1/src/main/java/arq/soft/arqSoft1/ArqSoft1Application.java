37af0a2727981e8682082b93127b099b7e4aa790
natirodriguez <nati.stefania@gmail.com>
2021-09-03 19:08:35 -0300

    Primer commit

---
+++ arqSoft1/src/main/java/arq/soft/arqSoft1/ArqSoft1Application.java
@@ -0,0 +1,13 @@
+package arq.soft.arqSoft1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArqSoft1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArqSoft1Application.class, args);
+	}
+
+}
---
+++ arqSoft1/src/test/java/arq/soft/arqSoft1/ArqSoft1ApplicationTests.java
@@ -0,0 +1,13 @@
+package arq.soft.arqSoft1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArqSoft1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
