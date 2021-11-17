fc75e1f53f7f447389b428128908c4906a17d120
natirodriguez <nati.stefania@gmail.com>
2021-11-17 18:23:08 -0300

    Creo exception producto sin stock

---
+++ arqSoft1/src/main/java/arq/soft/arqSoft1/excepciones/ProductoSinStockException.java
@@ -0,0 +1,10 @@
+package arq.soft.arqSoft1.excepciones;
+
+public class ProductoSinStockException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4629323034879799245L;
+
+}
